1e16a60c1bc5d9c7e967743d6ac829f328dad923
CXCubeHD <cxcubehd@gmail.com>
2021-08-18 21:41:59 +0200

    .

--- main.cc
+++ main.cc
@@ -14,72 +14,16 @@
 using namespace Index;
 using namespace Index::UI;
 
-unsigned __int64 counter = 0;
-
-class A
-{
-public:
-    __int64 i;
-    __int64 i2;
-    __int64 i3;
-    __int64 i4;
-    virtual void CallNext(int until, A a1, A a2) {
-        if (until <= 0) return;
-        counter += 1;
-        if (counter == 18446744073709551615 - 10) throw "Limit reached.";
-        i = until;
-        i2 = until;
-        i3 = until;
-        i4 = until;
-        auto a = A();
-        auto a5 = A();
-        a.CallNext(until - 1, a5, a);
-    }
-};
 
 int main()
 {
+
     UPtr<ImUI::ImUIContext> context = UNew<ImUI::ImUIContext>();
 
     auto ui = UI::Stack n ({
         StackV n ({
             alignment Center,
             content {
-                StackH n ({
-                    content {
-                        ImUI::FillRect n ({
-                            .Size { 160, 100 },
-                            .Fill = Colors::Blue
-                        }),
-                        ImUI::FillRect n ({
-                            .Size { 160, 100 },
-                            .Fill = Colors::Red
-                        })
-                    }
-                }),
-                FixedGrid n ({
-                    .Size { NullF, 100 },
-                    .Columns = 3,
-                    content {
-                        FixedGridElement n ({
-                            .Column = 0,
-                            .ColumnSpan = 3,
-                            content {
-                                ImUI::FillRect n ({
-                                    .Fill = Colors::Purple
-                                })
-                            }
-                        }),
-                        FixedGridElement n ({
-                            .Column = 0,
-                            content {
-                                ImUI::FillRect n ({
-                                    .Fill = Colors::Green
-                                })
-                            }
-                        })
-                    }
-                }),
                 Container n ({
                     .MinSize { 400, 400 },
                     content {

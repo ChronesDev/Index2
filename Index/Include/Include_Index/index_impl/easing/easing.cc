dcbf40c663697ce7c3eaac55cb8027604be46f24
CXCubeHD <cxcubehd@gmail.com>
2021-11-11 20:26:38 +0100

    fixed linker errors

--- Index/Include/Include_Index/index_impl/easing/easing.cc
+++ Index/Include/Include_Index/index_impl/easing/easing.cc
@@ -11,35 +11,35 @@ namespace Index::Easing
 {
     constexpr auto PI = std::numbers::pi;
 
-    double cos(double v) { return ::cos(v); }
-    double sin(double v) { return ::sin(v); }
-    double pow(double v1, double v2) { return ::pow(v1, v2); }
-    double sqrt(double v) { return ::sqrt(v); }
+    inline double cos(double v) { return ::cos(v); }
+    inline double sin(double v) { return ::sin(v); }
+    inline double pow(double v1, double v2) { return ::pow(v1, v2); }
+    inline double sqrt(double v) { return ::sqrt(v); }
 
-    double EaseInSine(double x) { return 1 - cos((x * PI) / 2); }
-    double EaseOutSine(double x) { return sin((x * PI) / 2); }
-    double EaseInOutSine(double x) { return -(cos(PI * x) - 1) / 2; }
-    double EaseInCubic(double x) { return x * x * x; }
-    double EaseOutCubic(double x) { return 1 - pow(1 - x, 3); }
-    double EaseInOutCubic(double x) { return x < 0.5 ? 4 * x * x * x : 1 - pow(-2 * x + 2, 3) / 2; }
-    double EaseInQuint(double x) { return x * x * x * x * x; }
-    double EaseOutQuint(double x) { return 1 - pow(1 - x, 5); }
-    double EaseInOutQuint(double x) { return x < 0.5 ? 16 * x * x * x * x * x : 1 - pow(-2 * x + 2, 5) / 2; }
-    double EaseInCirc(double x) { return 1 - sqrt(1 - pow(x, 2)); }
-    double EaseOutCirc(double x) { return sqrt(1 - pow(x - 1, 2)); }
-    double EaseInOutCirc(double x)
+    inline double EaseInSine(double x) { return 1 - cos((x * PI) / 2); }
+    inline double EaseOutSine(double x) { return sin((x * PI) / 2); }
+    inline double EaseInOutSine(double x) { return -(cos(PI * x) - 1) / 2; }
+    inline double EaseInCubic(double x) { return x * x * x; }
+    inline double EaseOutCubic(double x) { return 1 - pow(1 - x, 3); }
+    inline double EaseInOutCubic(double x) { return x < 0.5 ? 4 * x * x * x : 1 - pow(-2 * x + 2, 3) / 2; }
+    inline double EaseInQuint(double x) { return x * x * x * x * x; }
+    inline double EaseOutQuint(double x) { return 1 - pow(1 - x, 5); }
+    inline double EaseInOutQuint(double x) { return x < 0.5 ? 16 * x * x * x * x * x : 1 - pow(-2 * x + 2, 5) / 2; }
+    inline double EaseInCirc(double x) { return 1 - sqrt(1 - pow(x, 2)); }
+    inline double EaseOutCirc(double x) { return sqrt(1 - pow(x - 1, 2)); }
+    inline double EaseInOutCirc(double x)
     {
         return x < 0.5 ? (1 - sqrt(1 - pow(2 * x, 2))) / 2 : (sqrt(1 - pow(-2 * x + 2, 2)) + 1) / 2;
     }
-    double EaseInQuad(double x) { return x * x; }
-    double EaseOutQuad(double x) { return 1 - (1 - x) * (1 - x); }
-    double EaseInOutQuad(double x) { return x < 0.5 ? 2 * x * x : 1 - pow(-2 * x + 2, 2) / 2; }
-    double EaseInQuart(double x) { return x * x * x * x; }
-    double EaseOutQuart(double x) { return 1 - pow(1 - x, 4); }
-    double EaseInOutQuart(double x) { return x < 0.5 ? 8 * x * x * x * x : 1 - pow(-2 * x + 2, 4) / 2; }
-    double EaseInExpo(double x) { return x == 0 ? 0 : pow(2, 10 * x - 10); }
-    double EaseOutExpo(double x) { return x == 1 ? 1 : 1 - pow(2, -10 * x); }
-    double EaseInOutExpo(double x)
+    inline double EaseInQuad(double x) { return x * x; }
+    inline double EaseOutQuad(double x) { return 1 - (1 - x) * (1 - x); }
+    inline double EaseInOutQuad(double x) { return x < 0.5 ? 2 * x * x : 1 - pow(-2 * x + 2, 2) / 2; }
+    inline double EaseInQuart(double x) { return x * x * x * x; }
+    inline double EaseOutQuart(double x) { return 1 - pow(1 - x, 4); }
+    inline double EaseInOutQuart(double x) { return x < 0.5 ? 8 * x * x * x * x : 1 - pow(-2 * x + 2, 4) / 2; }
+    inline double EaseInExpo(double x) { return x == 0 ? 0 : pow(2, 10 * x - 10); }
+    inline double EaseOutExpo(double x) { return x == 1 ? 1 : 1 - pow(2, -10 * x); }
+    inline double EaseInOutExpo(double x)
     {
         return x == 0 ? 0 : x == 1 ? 1 : x < 0.5 ? pow(2, 20 * x - 10) / 2 : (2 - pow(2, -20 * x + 10)) / 2;
     }

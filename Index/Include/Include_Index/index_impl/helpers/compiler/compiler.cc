75dbbec141c46088ff2a324ef9b63f66fe46c3b2
CXCubeHD <cxcubehd@gmail.com>
2021-11-26 23:19:42 +0100

    updated compiler macros

--- Index/Include/Include_Index/index_impl/helpers/compiler/compiler.cc
+++ Index/Include/Include_Index/index_impl/helpers/compiler/compiler.cc
@@ -1,18 +1,38 @@
 #pragma once
 
-#if defined(_MSC_VER)
-#define INDEX_IsMSVC
-#define INDEX_CompilerName "MSVC"
+// Determines whether the current compiler is supported
+#define INDEX_SupportedCompiler
 
-#elif defined(__clang__)
-#define INDEX_IsClang
+// Check for Clang compiler
+#if defined(__clang__)
+#define INDEX_Clang
 #define INDEX_CompilerName "Clang"
 
+// Check for GNU compiler
 #elif defined(__GNUC__) || defined(__GNUG__)
-#define INDEX_IsGNU
+#define INDEX_GNU
 #define INDEX_CompilerName "GNU"
 
+// Check for MSVC compiler
+#elif defined(_MSC_VER)
+#define INDEX_MSVC
+#define INDEX_CompilerName "MSVC"
+
+// Custom Supported Compiler
+#elifdef INDEX_CustomCompiler
+#define INDEX_CompilerName "Custom"
+
+// Not Supported
 #else
-#define INDEX_IsUnknown
-#define INDEX_CompilerName "Unkown"
+#undef INDEX_SupportedCompiler
+#define INDEX_UnsupportedCompiler
+#define INDEX_CompilerName "Unknown"
+
+/**
+ * The current compiler you are currently using is not supported.
+ * Make sure that the current compiler has the needed feature support.
+ * If it has then define INDEX_CustomCompiler globally to suppress this error
+ */
+#error index: Unsupported compiler. To disable this error, define INDEX_CustomCompiler globally
+
 #endif
--- Index/Include/Include_Index/index_impl/helpers/property/property.cc
+++ Index/Include/Include_Index/index_impl/helpers/property/property.cc
@@ -1,12 +1,19 @@
 #pragma once
 
-// Check for Clang, MSVC or GCC
-#if defined(__clang__) || defined(_MSC_VER) || defined(__GNUC__) || defined(__GNUG__)
+#include "../compiler/compiler.cc"
 
+// Check for a supported compiler
+#ifdef INDEX_SupportedCompiler
+
+// Compiler is supported
 #define INDEX_Property(...) __declspec(property(__VA_ARGS__))
 
+// Compiler is not supported
 #else
 
-#error "It looks like you are using an unsupported compiler"
+/**
+ * The C++ Properties extension is not available. Make sure to use a supported compiler.
+ */
+#error index: The C++ Properties extension is not available
 
 #endif

262457b577b444d903de296ba8563a759fdfa5bc
CXCubeHD <cxcubehd@gmail.com>
2021-11-28 10:59:03 +0100

    .

--- Index/Include/Include_IndexUI/index_ui_impl/ui/builder/builder.cc
+++ Index/Include/Include_IndexUI/index_ui_impl/ui/builder/builder.cc
@@ -4,79 +4,96 @@
 
 namespace Index::SomeUI
 {
-    struct Element;
-    struct ElementMapper;
+	struct Element;
+	struct ElementMapper;
 
-    struct Element
-    {
-        template <class T> struct Map
-        {
-            Func<void(Element*, IPtr<ElementMapper>)> operator+(Func<void(Element*, const ElementMapper&)> f)
-            {
-                return [] {
-
-                };
-            }
-        };
-    };
+	struct Element
+	{
+		using Mapper = Element;
+	};
 
 #define sub element.template Sub <
-#define map > () += [](auto element, auto mapper)
-#define set element.
-#define get element.
+#define map > () += [this](auto parent, auto mapper)
+#define mapc(capture) > () += [capture](auto parent, auto mapper)
+#define set mapper.
+#define get mapper.
 
 	struct ElementMapper
-    {
-    public:
-        IPtr<ElementMapper> Content;
+	{
+	public:
+		IPtr <ElementMapper> Content;
 
 		string Name;
 
-        // Content
-        template <class TThis, class T> struct PlusAssignMaker
-        {
-            TThis* That;
-            explicit PlusAssignMaker(TThis* that)
-                : That(that)
-            {
-            }
-            void operator+=(Func<void(const TThis&, const typename T::Mapper&)> f)
-            {
-                auto mapper = INew<typename T::Mapper>();
-                f(*That, mapper.Value);
-                mapper.template DynamicAs<ElementMapper>();
-            }
-        };
+		// Content
+		template<class TThis, class T>
+		struct PlusAssignMaker
+		{
+			TThis *That;
 
-        template <class T> PlusAssignMaker<ElementMapper, T> Sub() { return PlusAssignMaker<ElementMapper, T>(this); }
+			explicit PlusAssignMaker(TThis *that) : That(that)
+			{
+			}
 
-        IPtr<Element> Build() { return { }; }
-    };
+			void operator+=(Func<void(const TThis &, const typename T::Mapper &)> f)
+			{
+				auto mapper = INew<typename T::Mapper>();
+				f(*That, mapper.Value);
+				mapper.template DynamicAs<ElementMapper>();
+			}
+		};
 
-    struct ButtonMapper;
+		template<class T>
+		PlusAssignMaker<ElementMapper, T> Sub()
+		{ return PlusAssignMaker<ElementMapper, T>(this); }
 
-    struct Button : Element
-    {
-        using Mapper = ButtonMapper;
-    };
+		IPtr <Element> Build()
+		{ return { }; }
+	};
 
-    struct ButtonMapper : ElementMapper
-    {
-    };
+	struct ButtonMapper;
 
-    void Make()
-    {
-        ElementMapper element;
-        sub Button map
-        {
-			set Name = "Hello There!";
-            sub Button map
-            {
-                sub Button map {
+	struct Button : Element
+	{
+		using Mapper = ButtonMapper;
+	};
 
-                };
-            };
-        };
-		auto ui = element.Build();
-    }
+	struct ButtonMapper : ElementMapper
+	{
+		int SomeValue = 0;
+	};
+
+#define n void
+
+	class MyClass
+	{
+		int A = 0;
+
+		void Make()
+		{
+			ElementMapper element;
+			sub Button map -> n
+			{
+				set Name = "Hello There!";
+				sub Element map -> n
+				{
+					sub Button map -> n
+					{
+						set SomeValue = 11;
+						set Name = "Hi?";
+						set Name = "";
+					};
+					sub Button map -> n
+					{
+						set Name = "Test";
+					};
+					sub Button map -> n
+					{
+						set Name = "Hi?";
+					};
+				};
+			};
+			auto ui = element.Build();
+		}
+	};
 }

6fb5820cb9768660c98e7e972ecc9a1626741718
CXCubeHD <cxcubehd@gmail.com>
2021-11-28 11:45:22 +0100

    some testing

--- Index/Include/Include_IndexUI/index_ui_impl/ui/builder/builder.cc
+++ Index/Include/Include_IndexUI/index_ui_impl/ui/builder/builder.cc
@@ -4,96 +4,131 @@
 
 namespace Index::SomeUI
 {
-	struct Element;
-	struct ElementMapper;
+    struct Element;
+    struct ElementMapper;
 
-	struct Element
-	{
-		using Mapper = Element;
-	};
+    struct Element
+    {
+        using Mapper = Element;
+    };
 
-#define sub element.template Sub <
+#define sub parent.template Sub <
 #define map > () += [this](auto parent, auto mapper)
+#define mapn > () += [](auto parent, auto mapper)
 #define mapc(capture) > () += [capture](auto parent, auto mapper)
 #define set mapper.
 #define get mapper.
 
-	struct ElementMapper
-	{
-	public:
-		IPtr <ElementMapper> Content;
+    struct ElementMapper
+    {
+    public:
+        List<IPtr<ElementMapper>> Content;
 
-		string Name;
+        string Name;
 
-		// Content
-		template<class TThis, class T>
-		struct PlusAssignMaker
-		{
-			TThis *That;
+        // Content
+        template <class TThis, class T> struct PlusAssignMaker
+        {
+            TThis* That;
 
-			explicit PlusAssignMaker(TThis *that) : That(that)
-			{
-			}
+            explicit PlusAssignMaker(TThis* that)
+                : That(that)
+            {
+            }
 
-			void operator+=(Func<void(const TThis &, const typename T::Mapper &)> f)
-			{
-				auto mapper = INew<typename T::Mapper>();
-				f(*That, mapper.Value);
-				mapper.template DynamicAs<ElementMapper>();
-			}
-		};
+            // Add Child
+            void operator+=(Func<void(const TThis&, const typename T::Mapper&)> f)
+            {
+                auto mapper = INew<typename T::Mapper>();
+                f(*That, mapper.Value);
+                That->Content.Add(mapper.template DynamicAs<ElementMapper>());
+            }
+        };
 
-		template<class T>
-		PlusAssignMaker<ElementMapper, T> Sub()
-		{ return PlusAssignMaker<ElementMapper, T>(this); }
+        // Get PlusAssignMaker
+        template <class T> PlusAssignMaker<ElementMapper, T> Sub() { return PlusAssignMaker<ElementMapper, T>(this); }
 
-		IPtr <Element> Build()
-		{ return { }; }
-	};
+        IPtr<Element> Build() { return {}; }
+    };
 
-	struct ButtonMapper;
+    struct ButtonMapper : ElementMapper
+    {
+        int SomeValue = 0;
+    };
 
-	struct Button : Element
-	{
-		using Mapper = ButtonMapper;
-	};
-
-	struct ButtonMapper : ElementMapper
-	{
-		int SomeValue = 0;
-	};
+    struct Button : Element
+    {
+        using Mapper = ButtonMapper;
+    };
 
 #define n void
 
-	class MyClass
-	{
-		int A = 0;
+    struct MyClassMapper : ElementMapper
+    {
+    };
 
-		void Make()
-		{
-			ElementMapper element;
-			sub Button map -> n
-			{
-				set Name = "Hello There!";
-				sub Element map -> n
-				{
-					sub Button map -> n
-					{
-						set SomeValue = 11;
-						set Name = "Hi?";
-						set Name = "";
-					};
-					sub Button map -> n
-					{
-						set Name = "Test";
-					};
-					sub Button map -> n
-					{
-						set Name = "Hi?";
-					};
-				};
-			};
-			auto ui = element.Build();
-		}
-	};
+    struct MyClass : Element
+    {
+        using Mapper = MyClassMapper;
+
+        int A = 0;
+
+        void Make()
+        {
+            ElementMapper parent;
+            sub Button map
+            {
+                A = 11;
+                set Name = "Hello There!";
+                sub Element mapn
+                {
+                    sub Button mapn
+                    {
+                        set Name = "Test";
+                        sub MyClass mapn
+                        {
+                            set Name = "??";
+                            set Name = "...";
+                        };
+                    };
+                    sub Button mapn
+                    {
+                        set SomeValue = 11;
+                        set Name = "Hi?";
+                        set Name = "";
+                    };
+                    sub Button mapn {};
+                    sub MyClass mapn
+                    {
+                        set Name = "?";
+                        sub MyClass mapn { string name = get Name; };
+                        sub MyClass mapn
+                        {
+                            set Name = "Hi";
+                            set Name = "Hi";
+                        };
+                        sub Button mapn
+                        {
+                            set Name = "L";
+                            set Name = "Hi";
+                        };
+                    };
+                };
+            };
+
+            /*
+             * Button
+             * -- Element
+             * -- -- Button
+             * -- -- -- MyClass
+             * -- -- Button
+             * -- -- Button
+             * -- -- MyClass
+             * -- -- -- MyClass
+             * -- -- -- MyClass
+             * -- -- -- Button
+             */
+            auto ui = parent.Build();
+        }
+    };
 }

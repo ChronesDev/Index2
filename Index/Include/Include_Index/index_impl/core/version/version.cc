e73a9da1912c832a88f0453918e358595d3b9074
CXCubeHD <cxcubehd@gmail.com>
2021-12-18 15:33:05 +0100

    added Index::Version comparisons

--- Index/Include/Include_Index/index_impl/core/version/version.cc
+++ Index/Include/Include_Index/index_impl/core/version/version.cc
@@ -12,6 +12,8 @@ namespace Index
         short Revision = 0;
 
         bool operator==(Version const&) const = default;
+        inline bool operator>(Version const&) const;
+        inline bool operator<(Version const&) const;
         inline operator string() const;
 
         inline static Version Parse(string s);
@@ -24,6 +26,36 @@ namespace Index
         std::stringstream ss;
         ss << Major << "." << Minor << "." << Build << "." << Revision;
         return ss.str();
+    }
+
+    inline bool Version::operator>(const Version& other) const
+    {
+        if (Major > other.Major) return true;
+        if (Major < other.Major) return false;
+
+        if (Minor > other.Minor) return true;
+        if (Minor < other.Minor) return false;
+
+        if (Build > other.Build) return true;
+        if (Build < other.Build) return false;
+
+        if (Revision > other.Revision) return true;
+        return false;
+    }
+
+    inline bool Version::operator<(const Version& other) const
+    {
+        if (Major < other.Major) return true;
+        if (Major > other.Major) return false;
+
+        if (Minor < other.Minor) return true;
+        if (Minor > other.Minor) return false;
+
+        if (Build < other.Build) return true;
+        if (Build > other.Build) return false;
+
+        if (Revision < other.Revision) return true;
+        return false;
     }
 
     Version Version::Parse(string s)

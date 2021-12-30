4b99cfc23f403990ff4ce46cf4f4a9d4c30e9def
CXCubeHD <cxcubehd@gmail.com>
2021-12-30 20:49:20 +0100

    + Limit + LerpC + LerpLimit + LerpL

--- Index/Include/Include_Index/index_impl/std/math/math.cc
+++ Index/Include/Include_Index/index_impl/std/math/math.cc
@@ -13,5 +13,19 @@ namespace Index
 
     template <class T> inline constexpr T Lerp(T a, T b, T v) { return std::lerp(a, b, v); }
 
-    template <class T> inline constexpr T LerpClamp(T a, T b, T v) { return Clamp(std::lerp(a, b, v), a, b); }
+    template <class T> inline constexpr T LerpClamp(T a, T b, T v) { return Clamp(Lerp(a, b, v), a, b); }
+
+    template <class T> inline constexpr T LerpC(T a, T b, T v) { return LerpClamp(a, b, v); }
+
+    template <class T> inline constexpr T Limit(T v, T a, T b)
+    {
+        if constexpr (a > b)
+            return Clamp(v, b, a);
+        else
+            return Clamp(v, a, b);
+    }
+
+    template <class T> inline constexpr T LerpLimit(T a, T b, T v) { return Limit(Lerp(a, b, v), a, b); }
+
+    template <class T> inline constexpr T LerpL(T a, T b, T v) { return LerpLimit(a, b, v); }
 }

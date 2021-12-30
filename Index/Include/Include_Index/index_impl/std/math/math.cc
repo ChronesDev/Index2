04de510b282b7669b08a8ac7795e049fd6f76fbb
CXCubeHD <cxcubehd@gmail.com>
2021-12-30 20:11:52 +0100

    added LerpClamp

--- Index/Include/Include_Index/index_impl/std/math/math.cc
+++ Index/Include/Include_Index/index_impl/std/math/math.cc
@@ -12,4 +12,6 @@ namespace Index
     template <class T> inline constexpr T Clamp(T v, T a, T b) { return std::clamp(v, a, b); }
 
     template <class T> inline constexpr T Lerp(T a, T b, T v) { return std::lerp(a, b, v); }
+
+    template <class T> inline constexpr T LerpClamp(T a, T b, T v) { return Clamp(std::lerp(a, b, v), a, b); }
 }

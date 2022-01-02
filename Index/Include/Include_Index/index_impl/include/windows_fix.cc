65e575b4df0e549543215f7a8c774678e5d5405c
CXCubeHD <cxcubehd@gmail.com>
2022-01-02 19:12:24 +0100

    made Index Linux ready

--- Index/Include/Include_Index/index_impl/include/windows_fix.cc
+++ Index/Include/Include_Index/index_impl/include/windows_fix.cc
@@ -1,5 +1,13 @@
 #ifndef INDEX_NO_WINDOWS_H
 
+#if !__has_include(<Windows.h>)
+#define INDEX_NO_WINDOWS_H
+#endif
+
+#endif
+
+#ifndef INDEX_NO_WINDOWS_H
+
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
 #endif

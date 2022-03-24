ddb121fc28a8cf1d94fdea3525aae340a77f3787
CXCubeHD <cxcubehd@gmail.com>
2022-03-24 21:26:34 +0100

    updated UIInputContext

--- Index/Include/Include_IndexUI/index_ui_impl/ui_input/context/context.cc
+++ Index/Include/Include_IndexUI/index_ui_impl/ui_input/context/context.cc
@@ -9,26 +9,35 @@ namespace Index::UI
     template <class T> struct UIInputContext : IUIInputContext<T>
     {
     protected:
+        UIInputElement<T>* IInputElement_;
+
+        void SetIInputElement_(UIElement* e)
+        {
+            if (e == nullptr) IInputElement_ = nullptr;
+            if (auto ptr = dynamic_cast<UIInputElement<T>*>(e); ptr)
+                IInputElement_ = ptr;
+            else
+                INDEX_THROW("Bad type.");
+        }
+
+    protected:
         void UIInputElement_Focus_(const IPtr<UIElement>& e) override
         {
             if (this->HasFocusedElement)
             {
-                if (auto ptr = this->FocusedElement_.template DynamicAs<UIInputElement<T>>(); ptr)
-                {
-                    ptr->Unfocus();
-                }
+                if (auto ptr = this->FocusedElement_.template DynamicAs<UIInputElement<T>>(); ptr) { ptr->Unfocus(); }
             }
+            SetIInputElement_(e.Ptr);
             this->FocusedElement_ = e;
         }
         void UIInputElement_Unfocus_() override
         {
             if (this->HasFocusedElement)
             {
-                if (auto ptr = this->FocusedElement_.template DynamicAs<UIInputElement<T>>(); ptr)
-                {
-                    ptr->Unfocus();
-                }
+                if (auto ptr = this->FocusedElement_.template DynamicAs<UIInputElement<T>>(); ptr) { ptr->Unfocus(); }
             }
+            SetIInputElement_(nullptr);
+            this->FocusedElement_ = nullptr;
         }
 
     public:
@@ -47,22 +56,33 @@ namespace Index::UI
         {
             if (this->HasFocusedElement)
             {
-                if (auto ptr = this->FocusedElement_.template DynamicAs<UIInputElement<T>>(); ptr)
-                {
-                    ptr->Unfocus();
-                }
+                if (auto ptr = this->FocusedElement_.template DynamicAs<UIInputElement<T>>(); ptr) { ptr->Unfocus(); }
             }
         }
 
     public:
-        virtual void EventKeyPress(typename T::TKey key) { }
-        virtual void EventKeyRelease(typename T::TKey key) { }
+        virtual void EventKeyPress(typename T::TKey key)
+        {
+            if (IInputElement_) { IInputElement_->EventKeyPress(key); }
+        }
+        virtual void EventKeyRelease(typename T::TKey key)
+        {
+            if (IInputElement_) { IInputElement_->EventKeyRelease(key); }
+        }
 
-        virtual void EventPointerMove(Vec2F from, Vec2F to) { }
-        virtual void EventPointerPress(typename T::TKey key) { }
-        virtual void EventPointerRelease(typename T::TKey key) { }
+        virtual void EventPointerMove(Vec2F from, Vec2F to)
+        {
+            if (IInputElement_) { IInputElement_->EventPointerMove(from, to); }
+        }
+        virtual void EventPointerPress(typename T::TKey key)
+        {
+            if (IInputElement_) { IInputElement_->EventPointerPress(key); }
+        }
+        virtual void EventPointerRelease(typename T::TKey key)
+        {
+            if (IInputElement_) { IInputElement_->EventPointerRelease(key); }
+        }
 
     public:
-
     };
 }
